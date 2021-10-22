54584a5ce60d0f2371096f66de464e7fa7713fcd
marymonique <mary99campbell@gmail.com>
2021-10-22 00:28:38 -0700

    Adding part 2 of app, creating a bottom navigation bar

--- app/src/main/java/com/codepath/mary9/parstagram/Post.java
+++ app/src/main/java/com/codepath/mary9/parstagram/Post.java
@@ -7,9 +7,11 @@ import com.parse.ParseUser;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
+
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_KEY = "createdAt";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
